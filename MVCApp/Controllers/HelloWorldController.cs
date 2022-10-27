85626d1705f6d6808b7e75eafab135c9cc8c638e
danwilliams92 <dan-ver.2007@hotmail.co.uk>
2022-10-27 22:18:33 +0100

    Add project files.

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCApp.Models;
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel doggy = new DogViewModel(){Name="tits", Age=4};
+            return View(doggy);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public string Hello()
+        {
+            return "Who is there?";
+        }
+    }
+}
